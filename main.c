8ad6c997d746ffe3b584007eff904ec8839b4464
tomgfang <77188687+kitchanat@users.noreply.github.com>
2022-02-07 21:57:40 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/*เก็บคะแนนครั้งที่4*/
+//ด.ช.กฤชณัท  แจ่มจำรัส
+//โปรแกรมหาพื้นที่รูปสามเหลี่ยม
+//1/2*b*h
+#include <stdio.h>
+int main()
+{
+    float base; //ฐาน
+    float height; //สูง
+    float pt; //คำตอบ
+    printf("\nInput value of base : ");
+    scanf("%f", &base);
+    printf("Input value of height : ");
+    scanf("%f", &height);
+    pt=0.5 * base * height;
+    printf("Triangle =%.2f",pt);
+}
